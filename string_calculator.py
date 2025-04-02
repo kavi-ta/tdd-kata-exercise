cc1bccc5fee0a0917c1d30ca08d18fb1b167876b
kavi-ta <kavisharma2807@gmail.com>
2025-04-02 22:54:41 +0530

    feat: to return an error on receiving no numbers

--- string_calculator.py
+++ string_calculator.py
@@ -1,4 +1,18 @@
 def string_calculator(string_of_nums):
-    if not string_of_nums.strip():
-        return 0
+    try:
+        if not string_of_nums.strip():
+            # empty string
+            return 0
+        else:
+            # clean data: only accept csv numbers
+            sum_value = 0
+            for num in string_of_nums.split(","):
+                if num=="":
+                    raise ValueError("Invalid Input")
+                sum_value+=int(num)
+
+            return sum_value
+    except Exception as e:
+        raise e
+
     
--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,10 @@ from string_calculator import string_calculator
 def test_add():
     assert string_calculator("") == 0
     assert string_calculator("  ")==0
+    assert string_calculator("1")==1
+    assert string_calculator("1,2")==3
+    assert string_calculator("10")==10
+    assert string_calculator("200")==200
+    with pytest.raises(ValueError) as excinfo:
+        string_calculator(",")
+    assert str(excinfo.value)== "Invalid Input"
